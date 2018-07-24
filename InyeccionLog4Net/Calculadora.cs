0c9dea0e2a12f3b2f397d1e018c4ad7c46175a79
gtd97 <genistorres97@gmail.com>
2018-07-24 14:34:12 +0200

    fin

---
+++ InyeccionLog4Net/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using log4net;
+
+namespace InyeccionLog4Net
+{
+    public class Calculadora : ICalculos
+    {
+        private readonly log4net.ILog _logger;
+
+        public Calculadora(ILog logger)
+        {
+            _logger = logger;
+        }
+
+
+        public int Division(int num1, int num2)
+        {
+            try
+            {
+                int res = num1 / num2;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("No se puede dividir entre 0. -> num1= "+ num1 + ", num = "+ num2);
+                throw new Exception("No se puede dividir entre 0.");
+            }
+        }
+
+    }
+}
---
+++ InyeccionLog4Net/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using log4net;
+
+namespace InyeccionLog4Net
+{
+    public class Program
+    {
+
+        private static IContainer Container { get; set; }
+        //static ILog Log;
+
+        static void Main(string[] args)
+        {
+            ConfigureContainer();
+
+            int num1 = 4000;
+            int num2 = 0;
+
+            Calcular(num1, num2);
+        }
+        
+        private static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Log
+            builder.RegisterModule(new LoggingModule());
+            //builder.RegisterModule<LoggingModule>();
+
+            // Clase calculo
+            builder.RegisterType<Calculadora>().SingleInstance();
+            
+            Container = builder.Build();
+        }
+
+        public static void Calcular(int num1, int num2)
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var res = scope.Resolve<Calculadora>().Division(num1, num2);
+                Console.WriteLine(res);
+            }
+        }
+
+       
+        
+    }
+}
